fc1829af3df325cc75920814110e04e3c7940041
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2020-07-12 11:34:25 -0300

    Add dockerfile and nginx configs

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7phxqi)8y^lwvy_smgxq*%43qhr(yz2s(hz)8&zq26dj)!#8cb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
